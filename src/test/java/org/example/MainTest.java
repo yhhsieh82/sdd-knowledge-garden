e2a663c504188fff93ac3be7a6162030f15c5480
nathan.hsieh <yi82121@gmail.com>
2026-02-08 09:22:14 +0000

    feature: initialize spring boot project

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainTest {
+
+    @Test
+    void contextLoads() {
+        // This test will fail if the Spring context cannot load
+        // No assertions needed - if context loads, test passes
+    }
+}
